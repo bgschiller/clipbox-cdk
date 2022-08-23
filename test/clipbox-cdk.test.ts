07bbb5801ef0d90df3c0b15dc0f3a547edec0c73
Brian Schiller <bgschiller@gmail.com>
2022-08-23 11:34:07 -0700

    Initial commit

---
+++ test/clipbox-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ClipboxCdk from '../lib/clipbox-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/clipbox-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ClipboxCdk.ClipboxCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
