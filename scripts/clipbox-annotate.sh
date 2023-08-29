b11f750fcb6132093d0194997a931ccabb4c98af
Brian Schiller <bgschiller@gmail.com>
2023-08-29 10:00:53 -0700

    update 'date' args for MacOS 13

--- scripts/clipbox-annotate.sh
+++ scripts/clipbox-annotate.sh
@@ -19,7 +19,7 @@ screencapture -i ~/.clipbox/capture.png
 open -W ~/.clipbox/capture.png
 # ignore illegal byte sequences from /dev/urandom
 export LC_ALL=C
-UPLOAD_NAME=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w 7 | head -n 1)-$(date -I date).png
-echo -n "https://$CLIPBOX_URL_PREFIX/$UPLOAD_NAME" | pbcopy
-aws --profile clipbox-writer s3 cp ~/.clipbox/capture.png s3://$CLIPBOX_URL_PREFIX/$UPLOAD_NAME --metadata-directive REPLACE --content-type image/png --acl public-read
+UPLOAD_NAME=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w 7 | head -n 1)-$(date -Idate).png
+echo -n "https://${CLIPBOX_URL_PREFIX:-clip.brianschiller.com}/$UPLOAD_NAME" | pbcopy
+aws --profile clipbox-writer s3 cp ~/.clipbox/capture.png s3://${CLIPBOX_URL_PREFIX:-brianschiller-clipbox}/$UPLOAD_NAME --metadata-directive REPLACE --content-type image/png --acl public-read
 echo "Copied URL to clipboard"
--- scripts/clipbox-text-or-file.sh
+++ scripts/clipbox-text-or-file.sh
@@ -19,10 +19,10 @@ FILENAME=$(osascript clip-text-or-file.applescript)
 if [[ $FILENAME == ~/.clipbox/clip.txt ]]; then
   # ignore illegal byte sequences from /dev/urandom
   export LC_ALL=C
-  UPLOAD_NAME=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w 7 | head -n 1)-$(date -I date).txt
+  UPLOAD_NAME=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w 7 | head -n 1)-$(date -Idate).txt
 else
   UPLOAD_NAME=$(basename $FILENAME)
 fi
-echo -n "https://$CLIPBOX_URL_PREFIX/$UPLOAD_NAME" | pbcopy
-aws --profile clipbox-writer s3 cp $FILENAME s3://$CLIPBOX_URL_PREFIX/$UPLOAD_NAME --metadata-directive REPLACE --content-type $(file --mime-type --brief $FILENAME) --acl public-read
+echo -n "https://${CLIPBOX_URL_PREFIX:-clip.brianschiller.com}/$UPLOAD_NAME" | pbcopy
+aws --profile clipbox-writer s3 cp $FILENAME s3://${CLIPBOX_URL_PREFIX:-brianschiller-clipbox}/$UPLOAD_NAME --metadata-directive REPLACE --content-type $(file --mime-type --brief $FILENAME) --acl public-read
 echo "Copied URL to clipboard"
